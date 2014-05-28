c99be5f978bbb98f84056a891f40c4441d33d90d
Paul Gray <pfbgray@gmail.com>
2014-05-28 14:33:12 -0400

    initial commit

---
+++ src/main/java/net/paulgray/mockltiapp/LtiController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.paulgray.mockltiapp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.imsglobal.spring.LtiLaunch;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author pgray
+ */
+@Controller
+public class LtiController {
+    
+    @LtiLaunch
+    @RequestMapping(value = {"/lti", "/test"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String ltiEntry(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("********************YOYOYOOYOY");
+        return "lti";
+    }
+    
+    @LtiLaunch(rejectIfBad = false, keyService = "keyService")
+    @RequestMapping(value = {"/another"}, method = {RequestMethod.POST, RequestMethod.GET})
+    public String ltiTest(HttpServletRequest request, ModelMap map, HttpServletResponse response){
+        System.out.println("********************in another");
+        return "lti";
+    }
+    
+}
---
+++ src/main/webapp/WEB-INF/views/lti.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : lti.jsp
+    Created on : May 24, 2014, 10:58:04 PM
+    Author     : pgray
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
