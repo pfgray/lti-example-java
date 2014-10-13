82ab1d88da75a7762fcfa20d3be072e1c67a3425
Paul Gray <pfbgray@gmail.com>
2014-10-13 11:41:11 -0400

    Adding run-time configuration for url choices/paths

---
+++ src/main/java/net/paulgray/exampleltiapp/config/LtiProviderConfig.java
@@ -0,0 +1,12 @@
+package net.paulgray.exampleltiapp.config;
+
+/**
+ * Created by pgray on 10/13/14.
+ */
+public class LtiProviderConfig {
+
+    public String default_base_url = "http://lti2-provider.paulgray.net";
+    public String secure_base_url = "http://lti2-provider.paulgray.net";
+    public String basic_lti_launch_path = "/lti";
+
+}
